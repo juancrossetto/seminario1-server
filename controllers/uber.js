45b08a471a6624f8284b076bd050ce15cabe5f7d
Alejandro <ortizalej199@gmail.com>
2021-05-31 21:10:44 -0300

    Uber

--- controllers/uber.js
+++ controllers/uber.js
@@ -2,16 +2,16 @@
 // get getAUTH
 exports.authUber = async (req, res) => {
     return res
-    .status(200)
-    .json({ token: '123' })
+        .status(200)
+        .json({ token: '123' })
 };
 
 exports.getEstimate = (req, res) => {
     return res
-    .status(200)
-    .json({ 
-        price: 123123,
-        time: 12
-    })
+        .status(200)
+        .json({
+            price: 24 * req.body.time,
+            time: 12
+        })
 };
 
